b029fafed35287274b9ae41996a9075925100a30
János Fehér <feherjani96@gmail.com>
2021-09-23 18:55:10 +0200

    Secret thing

--- events/message.js
+++ events/message.js
@@ -1,18 +1,22 @@
-const User = require('../database/models/user');
+const User = require("../database/models/user")
 
 module.exports = async (userMessage) => {
-    const user = await User.findOne({ where: { discordId: userMessage.author.id }});
+  const user = await User.findOne({
+    where: { discordId: userMessage.author.id },
+  })
 
-    if(!user)
-        await createUser(userMessage);
-    else
-        await user.update({ messageCount: user.messageCount + 1 });
-};
+  if (!user) await createUser(userMessage)
+  else
+    await user.update({
+      messageCount: user.messageCount + 1,
+      coin: user.coin + 1,
+    })
+}
 
 async function createUser(userMessage) {
-    await User.create({
-        discordId: userMessage.author.id,
-        username: userMessage.author.username,
-        messageCount: 1
-    });
+  await User.create({
+    discordId: userMessage.author.id,
+    username: userMessage.author.username,
+    messageCount: 1,
+  })
 }
