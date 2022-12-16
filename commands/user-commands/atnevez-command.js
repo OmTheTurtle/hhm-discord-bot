52cb9228b7b244a072710c94b40e97edc3237180
Janos Feher <feherjani96@gmail.com>
2022-12-16 02:23:27 +0100

    Restrict renaming

--- commands/user-commands/atnevez-command.js
+++ commands/user-commands/atnevez-command.js
@@ -1,16 +1,24 @@
 const User = require("../../database/models/user")
+const IDs = require("../../config/userIds.json");
 
 const price = 400
 
 module.exports = async (message) => {
   const user = await User.findOne({ where: { discordId: message.author.id } })
-  if (user.coin < price) {
+
+  if (!message.author.toString() !== IDs.DEVELOPER_GUNTHER_USER_ID && user.coin < price) {
     return message.channel.send(
       `${message.author.toString()}, túl csóró vagy az átnevezéshez (${price} tallér)`
     )
   }
 
   const userToRename = message.mentions.members.first()
+
+  if (userToRename.toString() === IDs.GYURI_USER_ID && message.author.toString() !== IDs.DEVELOPER_GUNTHER_USER_ID) {
+    return await message.channel.send(
+      `${message.author.toString()}, ezt neked nem szabad`
+    )
+  }
 
   if (message.mentions.members.array().length != 1) {
     return await message.channel.send(
